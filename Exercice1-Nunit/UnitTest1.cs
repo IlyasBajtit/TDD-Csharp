a35ac2c927845f387049fa2228223c02c869a82e
IlyasBajtit <ilyas.bajtit@gmail.com>
2024-01-24 14:57:45 +0100

    feat: add test

--- Exercice1-Nunit/UnitTest1.cs
+++ Exercice1-Nunit/UnitTest1.cs
@@ -1,16 +1,76 @@
-namespace Exercice1_Nunit
-{
-    public class Tests
-    {
-        [SetUp]
-        public void Setup()
-        {
-        }
+namespace Exercice1_Nunit;
+using NUnit.Framework;
 
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        }
+
+[TestFixture]
+public class GradingCalculatorTests
+{
+    private GradingCalculator _calculator;
+
+    [SetUp]
+    public void Setup()
+    {
+        _calculator = new GradingCalculator();
+    }
+
+    [Test]
+    public void GetGrade_WhenScoreIs95AndAttendanceIs90_ShouldReturnA()
+    {
+        _calculator.Score = 95;
+        _calculator.AttendancePercentage = 90;
+        var result = _calculator.GetGrade();
+        Assert.AreEqual('A', result);
+    }
+    [Test]
+    public void GetGrade_WhenScoreIs85AndAttendanceIs90_ShouldReturnB()
+    {
+        _calculator.Score = 85;
+        _calculator.AttendancePercentage = 90;
+        var result = _calculator.GetGrade();
+        Assert.AreEqual('B', result);
+    }
+
+    [Test]
+    public void GetGrade_WhenScoreIs65AndAttendanceIs90_ShouldReturnC()
+    {
+        _calculator.Score = 65;
+        _calculator.AttendancePercentage = 90;
+        var result = _calculator.GetGrade();
+        Assert.AreEqual('C', result);
+    }
+
+    [Test]
+    public void GetGrade_WhenScoreIs95AndAttendanceIs65_ShouldReturnB()
+    {
+        _calculator.Score = 95;
+        _calculator.AttendancePercentage = 65;
+        var result = _calculator.GetGrade();
+        Assert.AreEqual('B', result);
+    }
+    [Test]
+    public void GetGrade_WhenScoreIs95AndAttendanceIs55_ShouldReturnF()
+    {
+        _calculator.Score = 95;
+        _calculator.AttendancePercentage = 55;
+        var result = _calculator.GetGrade();
+        Assert.AreEqual('F', result);
+    }
+
+    [Test]
+    public void GetGrade_WhenScoreIs65AndAttendanceIs55_ShouldReturnF()
+    {
+        _calculator.Score = 65;
+        _calculator.AttendancePercentage = 55;
+        var result = _calculator.GetGrade();
+        Assert.AreEqual('F', result);
+    }
+
+    [Test]
+    public void GetGrade_WhenScoreIs50AndAttendanceIs90_ShouldReturnF()
+    {
+        _calculator.Score = 50;
+        _calculator.AttendancePercentage = 90;
+        var result = _calculator.GetGrade();
+        Assert.AreEqual('F', result);
     }
 }
