eb53508cf20ad4f6d5599a95a8444126e538c56a
IlyasBajtit <ilyas.bajtit@gmail.com>
2024-01-26 09:11:11 +0100

    feat: create and class NotFoundExecption

---
+++ Bibliotheque.Exercice03.MStest/NotFoundException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bibliotheque.Exercice03.MStest
+{
+    [Serializable]
+
+    public class NotFoundException : Exception
+
+    {
+        public NotFoundException()
+        {
+                
+        }
+
+        public NotFoundException(string message) : base(message)
+        {
+
+        }
+
+        public NotFoundException(string message, Exception inner) : base(message, inner)
+        {
+
+        }
+
+    }
+}
